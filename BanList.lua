0644f20ebaaab536dc0da90f08967d542e03fc64
TopGabBr <129920170+TopGabBr@users.noreply.github.com>
2023-05-02 13:43:05 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -19,6 +19,7 @@ local Ban = {
 	[3673993643] = {Motivo = "Abusando de comandos all."},
         [3008820693] = {Motivo = "amigo do p3u"},
 	[3184983208] = {Motivo = "Dando rank e admin para o cara que eu Fonojonzo acabei de tirar, vai ficar ban por muito tempo otário."},
+        [1510153167] = {Motivo = "Chatice"},
 	[538629407] = {Motivo = "HeadAdmin all"},
 	[2521671238] = {Motivo = "Gameban all"},
 	[1251812064] = {Motivo = "volta ai"},
