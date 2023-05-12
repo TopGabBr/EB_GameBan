333ad588383f366dfe49bb1266a90b819d2cea19
Fonojonzo <125100695+Fonojonzo@users.noreply.github.com>
2023-05-12 12:46:00 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -7,7 +7,6 @@ local Ban = {
 	[1214755295] = {Motivo = "Spammando ref all."}, 
 	[3088948388] = {Motivo = "Tirando rank de geral, não volta mais."},
 	[3963271291] = {Motivo = "Gameban all, não volta mais."},
-	[2499057894] = {Motivo = "'eb lixo'"},
 	[4326175331] = {Motivo = "Joga na EB do P3U."},
 	[2521671238] = {Motivo = "Suspeita de infiltrado."},
         [3000934539] = {Motivo = "Ameaçando"},
