65a6405d37576b5290f1586e819c904f9f29fd34
iExTno <132175289+iExTno@users.noreply.github.com>
2023-06-16 20:29:32 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -39,6 +39,7 @@ local Ban = {
 	[1661860127] = {Motivo = "vamos pensar em desbanir quando deletar os emblemas"}, -- 
 	--[2896747496] = {Motivo = "falta de robox"}, -- Aleatoriodomorro54
 	[375521456]  = {Motivo = " "}, -- roubo de conta
+	[4230561867] = {Motivo = " "}, -- 
 
 	
 }
