cef62ca81ebba6cae6956a40269a58dbe1266033
iExTno <132175289+iExTno@users.noreply.github.com>
2023-07-15 15:39:50 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -42,5 +42,6 @@ local Ban = {
 	[517391900] = {Motivo = "flw"}, -- dando rank all no v2
 	[263287460] = {Motivo = "flw"}, -- report do master, falando que tava ferrando o jogo todo, voando exploitando trollando as ebs;
 	[3375177854] = {Motivo = "Crime virtual"}, -- Assédio online?
+	[629259254] = {Motivo =
 }
 return Ban 
