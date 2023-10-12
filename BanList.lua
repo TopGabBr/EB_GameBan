1586185cd3c614af76e6718a596c6b691ab6f4cf
iExTno <132175289+iExTno@users.noreply.github.com>
2023-10-11 22:01:34 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -92,7 +92,7 @@ local Ban = {
 	[2995599962] = {Motivo = "troll"}, -- comandos all spammando
 	[1684970284] = {Motivo = "volte para 2017"}, -- tentando roubar conta fingindo que vai colocar robux
 	[4578959899] = {Motivo = "flw"}, -- ele simplesmente queria ser banido
-	
+	[13032042] = {Motivo = "aqui não né top"}, -- topgab entrando em conta fake pra trollar o jogo
 
 	
 }
