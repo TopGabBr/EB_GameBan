4703b0bc8959c42e12b9b88579d0f133eb0fb6c9
iExTno <132175289+iExTno@users.noreply.github.com>
2023-10-14 02:48:21 -0300

    Update BanList.lua

--- BanList.lua
+++ BanList.lua
@@ -95,6 +95,7 @@ local Ban = {
 	[13032042] = {Motivo = "aqui não né top"}, -- topgab entrando em conta fake pra trollar o jogo
 	[2005319633] = {Motivo = "flw"}, -- zoando no dc por n aceitar banimento por abusar no jogo
 	[4539404582] = {Motivo = "exploit"}, -- hack
+	[2622901376] = {Motivo = "flw"}, -- cumplice do hack
 	
 }
 return Ban 
